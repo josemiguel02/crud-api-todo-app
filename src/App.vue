ce1d47ba6e0d6c671b6bdcf7e0dc316a8e7949d0
josemiguel02 <jeffrey_m@hotmail.es>
2022-05-25 01:12:06 -0500

    add footer

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,34 @@
   <div id="app">
     <Navbar />
     <Tasks />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Tasks from './components/Tasks.vue'
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Tasks,
     Navbar,
-  },
+    Footer
+  }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    Made with <span class="heart">💚</span> by
+    <a href="https://github.com/josemiguel02" target="__blank" class="link">
+      José Miguel DP
+    </a>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 30px;
+  width: 100%;
+  padding: 1.5em;
+  font-size: 0.8rem;
+}
+
+.link {
+  text-decoration: none;
+  color: #fff;
+  background-color: #3bdec8;
+  padding: 5px;
+  border-radius: 10px;
+}
+</style>
