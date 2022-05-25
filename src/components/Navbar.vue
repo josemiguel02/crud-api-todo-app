93269558ddf60b69c8f6aaa1bae3bb424a03d424
josemiguel02 <jeffrey_m@hotmail.es>
2022-05-25 00:42:53 -0500

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,30 +1,16 @@
 <template>
-  <div class="center examplex">
-    <vs-navbar
-      :fixed="true"
-      text-white 
-      color="#3bdec8" 
-      center-collapsed 
-      v-model="active"
-      >
+  <div>
+    <vs-navbar :fixed="true" text-white color="#3bdec8" center-collapsed>
       <template #left>
-        <img src="https://img.icons8.com/fluent/50/000000/microsoft-todo-2019.png" />
+        <img
+          src="https://img.icons8.com/fluent/50/000000/microsoft-todo-2019.png"
+          alt="Logo"
+        />
       </template>
-      <vs-navbar-item :active="active == 'guide'" id="guide">
-        ToDO App
-      </vs-navbar-item>
-      <vs-navbar-item :active="active == 'docs'" id="docs">
-        Documents
-      </vs-navbar-item>
+
+      <template #right>
+        <h4>ToDo APP</h4>
+      </template>
     </vs-navbar>
   </div>
 </template>
-
-<script>
-export default {
-  name: 'Navbar',
-  data:() => ({
-      active: 'guide'
-  })
-};
-</script>
